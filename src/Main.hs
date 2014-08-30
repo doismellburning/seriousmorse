d3374f564209d99502dda8ba8a94be2b9b6b95a2
Kristian Glass <git@doismellburning.co.uk>
2014-08-30 15:12:12 +0200

    Start Arduino loop in iothread

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,17 @@
 
+import Control.Concurrent
 import Control.Monad
 import Data.Word
 import System.Hardware.Arduino
 import System.Hardware.Arduino.SamplePrograms.Morse
 
 main :: IO ()
-main = withArduino True "/dev/cu.usbmodemfd131" $ do
+main = do
+    putStrLn "Starting Arduino loop"
+    _ <- forkIO arduinoLoop
+    putStrLn "Running!"
+
+arduinoLoop = withArduino True "/dev/cu.usbmodemfd131" $ do
     let led = digital outPin
     setPinMode led OUTPUT
     forever $ transmit led "seriousmorse at emf"
