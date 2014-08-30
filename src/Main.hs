97f520915213e94edb31014b8c03c0de21843eb4
Kristian Glass <git@doismellburning.co.uk>
2014-08-30 16:39:23 +0200

    Fix warning

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ import Control.Concurrent
 import qualified Control.Concurrent.BoundedChan as BC
 import Control.Monad
 import Control.Monad.Trans (liftIO)
+import Data.Maybe
 import Data.Word
 import System.Hardware.Arduino
 import System.Hardware.Arduino.SamplePrograms.Morse
@@ -17,10 +18,6 @@ main = do
 
 channelSize = 10
 
-if' :: Bool -> a -> a -> a
-if' True x _ = x
-if' False _ x = x
-
 defaultMessage = "serious emf"
 
 arduinoLoop :: BC.BoundedChan String -> IO ()
@@ -28,7 +25,7 @@ arduinoLoop channel = withArduino True "/dev/cu.usbmodemfd131" $ do
     let led = digital outPin
     setPinMode led OUTPUT
     forever $ do
-        message <- liftIO $ liftM3 if' (BC.isEmptyChan channel) (return defaultMessage) (BC.readChan channel)
+        message <- liftIO $ fmap (fromMaybe defaultMessage) $ BC.tryReadChan channel
         transmit led message
 
 outPin :: Word8
