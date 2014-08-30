cd7645703461b02bd2f48e99c03dd63918c4bf58
Kristian Glass <git@doismellburning.co.uk>
2014-08-30 16:18:46 +0200

    Read messages from channel

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,35 @@
 
 import Control.Concurrent
+import qualified Control.Concurrent.BoundedChan as BC
 import Control.Monad
+import Control.Monad.Trans (liftIO)
 import Data.Word
 import System.Hardware.Arduino
 import System.Hardware.Arduino.SamplePrograms.Morse
 
 main :: IO ()
 main = do
+    putStrLn "Building channels"
+    channel <- BC.newBoundedChan channelSize
     putStrLn "Starting Arduino loop"
-    _ <- forkIO arduinoLoop
+    _ <- forkIO $ arduinoLoop channel
     putStrLn "Running!"
 
-arduinoLoop = withArduino True "/dev/cu.usbmodemfd131" $ do
+channelSize = 10
+
+if' :: Bool -> a -> a -> a
+if' True x _ = x
+if' False _ x = x
+
+defaultMessage = "serious emf"
+
+arduinoLoop :: BC.BoundedChan String -> IO ()
+arduinoLoop channel = withArduino True "/dev/cu.usbmodemfd131" $ do
     let led = digital outPin
     setPinMode led OUTPUT
-    forever $ transmit led "seriousmorse at emf"
+    forever $ do
+        message <- liftIO $ liftM3 if' (BC.isEmptyChan channel) (return defaultMessage) (BC.readChan channel)
+        transmit led message
 
 outPin :: Word8
 outPin = 12
